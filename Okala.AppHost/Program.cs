8a5c6fd595783cadfc3b93ac6297659ce1502b1a
raminsamdad <mh.samdad@gmail.com>
2024-10-08 13:04:41 +0300

    remove unnecessary files

--- Okala.AppHost/Program.cs
+++ Okala.AppHost/Program.cs
@@ -1,10 +1,8 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var cryptoApi = builder.AddProject<Projects.Crypto_Api>("cryptoApi");
 var apiAppService = builder.AddProject<Projects.Okala_Api_App>("apiAppservice");
-builder.AddProject<Projects.Okala_Web>("webfrontend")
+builder.AddProject<Projects.Crypto_Api>("cryptoApi")
     .WithExternalHttpEndpoints()
-    .WithReference(cryptoApi)
     .WithReference(apiAppService)
     ;
 
